a647e39e9f9c14387883f9e26326fa066d809458
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-26 10:58:29 -0700

    solution finished

--- app.js
+++ app.js
@@ -1,54 +1,94 @@
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('#result')
-let cardsChosen = []
-let cardsChosenId = []
+let cardsChosen = [] //objs
+let cardsChosenId = [] // strings
 let cardsWon = []
-
+let cardsCollection = new Map();
 const msgBoard = document.querySelector('.msg');
+const cards = document.querySelectorAll('.card');
 
 
-//check for matches
-function checkForMatch() {
-  const cards = document.querySelectorAll('img')
-  const optionOneId = cardsChosenId[0]
-  const optionTwoId = cardsChosenId[1]
-
-  if (optionOneId === optionTwoId) {
-    cards[optionOneId].setAttribute('src', 'images/blank.png')
-    cards[optionTwoId].setAttribute('src', 'images/blank.png')
-    msgBoard.innerText = 'You have clicked the same image!';
-  }
-  else if (cardsChosen[0] === cardsChosen[1]) {
-    msgBoard.innerText = 'You found a match';
-    cards[optionOneId].setAttribute('src', 'images/white.png')
-    cards[optionTwoId].setAttribute('src', 'images/white.png')
-    cards[optionOneId].removeEventListener('click', flipCard)
-    cards[optionTwoId].removeEventListener('click', flipCard)
-    cardsWon.push(cardsChosen)
-  } else {
-    cards[optionOneId].setAttribute('src', 'images/blank.png')
-    cards[optionTwoId].setAttribute('src', 'images/blank.png')
-    msgBoard.innerText = 'Sorry, try again';
-  }
-  cardsChosen = []
-  cardsChosenId = []
-  resultDisplay.textContent = cardsWon.length
-  if (cardsWon.length === cardArray.length / 2) {
-    resultDisplay.textContent = 'Congratulations! You found them all!'
-    // shuffle
-    cardArray.sort(() => 0.5 - Math.random())
-
-  }
+function startGame() {
+  cards.forEach((c) => {
+    const imgSrcArr = c.querySelector('.pic').src.split('/');
+    const fileName = imgSrcArr[imgSrcArr.length - 1];
+    const name = fileName.split('.')[0];
+    const id = c.getAttribute('data-id');
+    cardsCollection.set(id, { 'name': name, 'card': c });
+    c.addEventListener('click', flipCard);
+  });
 }
 
 //flip your card
 function flipCard() {
-  let cardId = this.getAttribute('data-id')
-  cardsChosen.push(cardArray[cardId].name)
-  cardsChosenId.push(cardId)
-  this.setAttribute('src', cardArray[cardId].img)
+  let cardId = this.getAttribute('data-id');
+  cardsChosen.push(cardsCollection.get(cardId));
+  cardsChosenId.push(cardId);
+  const cover = this.querySelector('.cover');
+  const img = this.querySelector('.pic');
+
+  show(img);
+  hide(cover);
+
   msgBoard.innerText = '';
+
   if (cardsChosen.length === 2) {
-    checkForMatch()
+      checkForMatch();
   }
 }
+
+function hide(e) {
+  // e.style.opacity = '0';
+  e.style.display = 'none';
+}
+
+function show(e) {
+  // e.style.opacity = '1';
+  e.style.display = 'block';
+}
+//check for matches
+function checkForMatch() {
+  const optionOneId = cardsChosenId[0]
+  const optionTwoId = cardsChosenId[1]
+
+  if (optionOneId === optionTwoId) {
+    const card = cardsCollection.get(optionOneId)['card'];
+    const cover = card.querySelector('.cover');
+    const img = card.querySelector('.pic');
+    msgBoard.innerText = 'You have clicked the same image!';
+
+    show(cover);
+    hide(img);
+
+  }
+  else if (cardsChosen[0]['name'] == cardsChosen[1]['name']) {
+    msgBoard.innerText = 'You found a match';
+    cardsWon.push(cardsChosen)
+  } else {
+    msgBoard.innerText = 'Sorry, try again';
+
+    const cardOne = cardsCollection.get(optionOneId)['card'];
+    const coverOne = cardOne.querySelector('.cover');
+    const imgOne = cardOne.querySelector('.pic');
+    show(coverOne);
+    hide(imgOne);
+
+
+    const cardTwo = cardsCollection.get(optionTwoId)['card'];
+    const coverTwo = cardTwo.querySelector('.cover');
+    const imgTwo = cardTwo.querySelector('.pic');
+    show(coverTwo);
+    hide(imgTwo);
+
+  }
+
+  cardsChosen = []
+  cardsChosenId = []
+  resultDisplay.textContent = cardsWon.length
+  if (cardsWon.length === cards.length / 2) {
+    resultDisplay.textContent = 'Congratulations! You found them all!'
+  }
+}
+
+
+startGame();
